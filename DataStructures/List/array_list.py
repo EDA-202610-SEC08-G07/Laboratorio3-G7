cedebaa3c0a6b013464447f4417c401b654adc20
jsaldanam <j.saldanam@uniandes.edu.co>
2026-02-11 16:01:15 -0500

    si

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -1,6 +1,31 @@
+from DataStructures.List import array_list as al
+
 def new_list():
-    newlist = {
+    new_list = {
         "elements": [],
         "size": 0,
     }
-    return newlist
+    return new_list
+
+def add_first(my_list,element):
+   
+    
+
+
+# Agrega un elemento al inicio
+    my_list = al.add_first(my_list,element )
+    print(my_list)
+
+def add_last(my_list,element):
+    
+
+
+    my_list = al.add_first(my_list, element)
+    print(my_list)
+
+def size(my_list):
+    print(al.size(my_list))
+    
+    
+def first_element(my_list):
+    print(al.first_element(my_list))
--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -1,7 +1,49 @@
+from DataStructures.List import single_linked_list as sll
+
 def new_list():
-    newlist = {
+    new_list = {
         "first": None,
         "last": None,
-        "size": None,
+        "size": 0,
     }
-    return newlist 
+    return new_list
+def get_element(my_list,pos):
+    searchpos = 0
+    node = my_list["first"]
+    while searchpos < pos :
+        node = node["next"]
+        searchpos += 1
+    return node["node"]
+def is_present(my_list,element,cmp_function):
+    is_in_array = False
+    temp = my_list["first"]
+    count = 0
+    while not is_in_array and temp is not None:
+        if cmp_function(element, temp["info"]) == 0:
+            is_in_array = True
+        else:
+            temp = temp["next"]
+            count += 1
+    if not is_in_array:
+        count = -1
+    return count 
+def add_first(my_list,element):
+    
+    my_list = sll.add_first(my_list,element)
+    print(my_list)
+
+def add_last(my_list,element):
+    
+    my_list = sll.add_last(my_list, element)
+    print(my_list)
+
+def size(my_list):
+    print(sll.size(my_list))
+    
+
+def first_element(my_list):
+    
+    
+    print(sll.first_element(my_list))
+       
+    

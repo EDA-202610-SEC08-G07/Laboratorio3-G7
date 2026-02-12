33f3ea959a8525a98a454e53fcadf4cb99c58ca8
Everth2804 <e.platae@uniandes.edu.co>
2026-02-11 19:56:17 -0500

    ARRAY_LIST CORREGIDO

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -41,7 +41,7 @@ def last_element(my_list):
     if is_empty(my_list):
        raise Exception('IndexError: list index out of range')
     else:
-        return my_list["elements"][size(my_list)]
+        return my_list["elements"][size(my_list) - 1]
 
 def delete_element(my_list, pos):
     if 0 <= pos < size(my_list):
@@ -52,23 +52,25 @@ def delete_element(my_list, pos):
     return my_list
 
 def remove_first(my_list):
-    if is_empty(my_list):
+    if my_list["size"] == 0:
         raise Exception('IndexError: list index out of range')
-    else: 
-        my_list["elements"].pop(0)
-        my_list["size"] -= 1
-    return my_list
+
+    my_list["size"] -= 1
+    return my_list["elements"].pop(0)
+    
 
 def remove_last(my_list):
-    if is_empty(my_list):
+    if my_list["size"] == 0:
         raise Exception('IndexError: list index out of range')
-    else: 
-        my_list["elements"].pop()
-        my_list["size"] -= 1
-    return my_list
+
+    my_list["size"] -= 1
+    return my_list["elements"].pop()
+    
 
 def insert_element(my_list, element, pos):
-    if is_empty(my_list):
+    if pos < 0 or pos > size(my_list):
+        raise Exception('IndexError: list index out of range')
+    elif is_empty(my_list):
         my_list["elements"].append(element)
     else:
         my_list["elements"].insert(pos, element)
@@ -76,23 +78,29 @@ def insert_element(my_list, element, pos):
     return my_list
 
 def change_info(my_list, pos, new_info):
-    if pos > size(my_list):
+    if pos < 0 or pos >= size(my_list):
         raise Exception('IndexError: list index out of range')
     else: 
         my_list["elements"][pos] = new_info
-    return my_list
+        return my_list
+    
 
 def exchange(my_list, pos_1, pos_2):
-    if pos_1 > size(my_list) or pos_2 > size(my_list):
+    if (pos_1 < 0 or pos_1 >= size(my_list) or pos_2 < 0 or pos_2 >= size(my_list)):
         raise Exception('IndexError: list index out of range')
     else:
         x = my_list["elements"][pos_2]
         my_list["elements"][pos_2] = my_list["elements"][pos_1]
         my_list["elements"][pos_1] = x
-    return my_list
+        return my_list
 
 def sub_list(my_list, pos_i, num_elements):
-    if pos_i > size(my_list):
+    if pos_i < 0 or pos_i >= my_list["size"]:
         raise Exception('IndexError: list index out of range')
-    else:
-        return my_list["elements"][pos_i:pos_i + num_elements]
+
+    new_elements = my_list["elements"][pos_i:pos_i + num_elements]
+
+    return {
+        "elements": new_elements,
+        "size": len(new_elements)
+    }
